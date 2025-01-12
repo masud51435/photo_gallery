e42c03560275b7e26cf4d2e5fd967763b7d052ca
MD MASUD RANA <86624203+masud51435@users.noreply.github.com>
2025-01-13 00:15:13 +0600

    setup complete

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:image_gallery/features/gallery_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Image Gallery',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: GalleryScreen(),
+    );
+  }
+}
